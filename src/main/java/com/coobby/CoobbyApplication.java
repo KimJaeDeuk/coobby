8f1f6287d5a41e260ed6152211b1cf1e7bbf1703
Hyungjun <gudwnd1243@gmail.com>
2022-05-16 12:34:47 +0900

    Add:project 생성

---
+++ src/main/java/com/coobby/CoobbyApplication.java
@@ -0,0 +1,13 @@
+package com.coobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoobbyApplication.class, args);
+	}
+
+}
