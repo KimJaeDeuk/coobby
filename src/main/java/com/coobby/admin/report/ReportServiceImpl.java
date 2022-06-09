c127162cafc175a3879aeb08bbb40b9afb22ad54
jihye9610 <97944247+jihye9610@users.noreply.github.com>
2022-06-09 09:39:10 +0900

    Add: 댓글기능구현, 대댓글 기능 구현중

--- src/main/java/com/coobby/admin/report/ReportServiceImpl.java
+++ src/main/java/com/coobby/admin/report/ReportServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.coobby.admin.dashboard.MemberRepository;
+import com.coobby.repository.MemberRepository;
 import com.coobby.repository.ReportRepository;
 import com.coobby.vo.MemberVO;
 import com.coobby.vo.ReportVO;
